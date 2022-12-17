0cbad987a9eed2c1e6a9635fa39af9950dd280aa
Djuro__pegla <kotlajastefan8@gmail.com>
2022-12-17 20:17:48 +0100

    registracija zavrsena

--- client.py
+++ client.py
@@ -3,6 +3,7 @@ import socket
 
 FORMAT = 'utf-8'    
 RESET = True
+MAX_TICKETS = int()
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('127.0.0.1', 9898))
@@ -44,6 +45,7 @@ def rezervacija():
 
 def recieve():
     global RESET
+    global MAX_TICKETS
     while RESET:
         try:
             msg = client.recv(1024).decode(FORMAT)
@@ -56,6 +58,10 @@ def recieve():
                 print(msg[7:])
             elif msg[:9] == 'BROADCAST':
                 print(msg[9:])
+            elif msg[:3] == 'ANN':
+                print(msg[3:])
+            elif msg[:11] == 'MAX_TICKETS':
+                MAX_TICKETS = int(msg[11:])
         except:
             print("Veza sa serverom je prekinuta")
             client.close()
@@ -71,40 +77,177 @@ def recieve():
 def meni():
     global RESET
     while RESET:
+        client.send('MAX_TICKETS'.encode(FORMAT))
         choice = f'{input()}'
         # choice = f'{input("Vas izbor: ")}'
         choice = choice.rstrip()
         if choice.upper() == 'LIST':
-            # list(choice)
+            # list(choice)  
             client.send(choice.encode(FORMAT))
         elif choice.upper() == 'RESERVE':
-            br = rezervacija()
+            # br = rezervacija()
             # reserve(choice+str(br))
+            print(f'Unesite broj karata koje zelite da rezervisete (mozete rezervisati {MAX_TICKETS}):')
+            while True:
+                try:
+                    br = int(input())
+                    break
+                except:
+                    print('Mozete unositi samo brojeve!')
             client.send((choice+str(br)).encode(FORMAT))
+            # client.send('MAX_TICKETS'.encode(FORMAT))
         elif choice.upper() == 'IZLAZ':
             RESET = False
             input()
             break
+        else:
+            print("Pogresan unos!")
 
 def auth_client():
     global RESET
-    thread_dead = False
-    print(client.recv(1024).decode(FORMAT))
-    while RESET:
+    auth_complete = False
+    # alive = True
+    # print(client.recv(1024).decode(FORMAT))
+    # alive = True
+    while True:
         try:
-            message = client.recv(1024).decode(FORMAT)
-            if message == 'Izaberite neku od opcija\nlogin - za postojeci nalog\nregister - za registraciju\nizlaz - za izlaz iz aplikacije':
-                unos = input()
-                client.send(unos.encode(FORMAT))
-                
+            message_rcvd = client.recv(1024).decode(FORMAT)
+            if message_rcvd[:3] == 'ANN':
+                print(message_rcvd[3:])
+            if message_rcvd[:8] == 'GREETING':
+                print(message_rcvd[8:])
+                while True:
+                    try:
+                        message = input().upper()
+                        if message == 'REGISTER':
+                            client.send('REG'.encode(FORMAT))
+                            break
+                        else:
+                            print('Greska! Pokusajte ponovo:')
+                    except:
+                        print('KONEKCIJA PREKIINUTA')
+                        client.close()
+
+            # if message_rcvd[:10] == 'REG_U_NAME':
+            #     print(message_rcvd[10:])
+            #     message = input()
+            #     if ((len(message) > 0) and (len(message) < 20)):
+            #         client.send(('U_NAME'+message).encode(FORMAT))
+            
+            if message_rcvd[:10] == 'REG_U_NAME':
+                print(message_rcvd[10:])
+                while True:
+                    try:
+                        message = input()
+                        if ((len(message) > 0) and (len(message) < 20)):
+                            client.send(('U_NAME'+message).encode(FORMAT))
+                            break
+                        else:
+                            print('Greska! Pokusajte ponovo:')
+                    except:
+                        print('KONEKCIJA PREKIINUTA')
+                        client.close()
+            
+            if message_rcvd[:12] == 'REG_PASSWORD':
+                print(message_rcvd[12:])
+                while True:
+                    try:
+                        message = input()
+                        if ((len(message) > 0) and (len(message) < 20)):
+                            client.send(('PASSWORD'+message).encode(FORMAT))
+                            break
+                        else:
+                            print('Greska! Pokusajte ponovo:')
+                    except:
+                        print('KONEKCIJA PREKIINUTA')
+                        client.close()
+            
+            if message_rcvd[:8] == 'REG_NAME':
+                print(message_rcvd[8:])
+                while True:
+                    try:
+                        message = input()
+                        if ((len(message) > 0) and (len(message) < 20)):
+                            client.send(('NAME'+message).encode(FORMAT))
+                            break
+                        else:
+                            print('Greska! Pokusajte ponovo:')
+                    except:
+                        print('KONEKCIJA PREKIINUTA')
+                        client.close()
+
+            if message_rcvd[:10] == 'REG_L_NAME':
+                print(message_rcvd[10:])
+                while True:
+                    try:
+                        message = input()
+                        if ((len(message) > 0) and (len(message) < 20)):
+                            client.send(('L_NAME'+message).encode(FORMAT))
+                            break
+                        else:
+                            print('Greska! Pokusajte ponovo: ')
+                    except:
+                        print('KONEKCIJA PREKIINUTA')
+                        client.close()
+
+            if message_rcvd[:8] == 'REG_JMBG':
+                print(message_rcvd[8:])
+                while True:
+                    try:
+                        while True:
+                            try:
+                                message = int(input())
+                                break
+                            except:
+                                print('Mozete unositi samo brojeve!')
+                        
+                        if (len(str(message)) == 13):
+                            client.send(('JMBG'+(str(message))).encode(FORMAT))
+                            break
+                        else:
+                            print('Greska! Pokusajte ponovo: ')
+                    except:
+                        print('KONEKCIJA PREKIINUTA')
+                        client.close()
+
+            if message_rcvd[:9] == 'REG_EMAIL':
+                print(message_rcvd[9:])
+                while True:
+                    try:
+                        message = input()
+                        if ((len(message) > 0) and (len(message) < 20)):
+                            client.send(('EMAIL'+message).encode(FORMAT))
+                            break
+                        else:
+                            print('Greska! Pokusajte ponovo: ')
+                    except:
+                        print('KONEKCIJA PREKIINUTA')
+                        client.close()
+
+            if message_rcvd[:12] == 'AUTH_SUCCESS':
+                # print(message_rcvd[12:])
+                print("EVE GA NA KRAJU")
+                auth_complete = True
+                break
+
+                # while alive:
+                #     message = input()
+                #     if ((len(message) > 0) and (len(message) < 20)):
+                #         client.send(('NAME'+message).encode(FORMAT))
+                #         alive = False
+                #     else:
+                #         print('Greska! Pokusajte ponovo: ')
+                #     alive = True
+            # if message == 'Izaberite neku od opcija\nlogin - za postojeci nalog\nregister - za registraciju\nizlaz - za izlaz iz aplikacije':
+            #     unos = input()
+            #     client.send(unos.encode(FORMAT)) 
         except:
             print("Veza sa serverom je prekinuta")
             client.close()
             thread_dead = True
             input()
             break
-
-    if thread_dead == False:
+    if auth_complete:
         recieve_thread = threading.Thread(target=recieve)
         meni_thread = threading.Thread(target=meni)
 
--- server.py
+++ server.py
@@ -17,26 +17,26 @@ username_list = []
 password_list = []
 ime_list = []
 prezime_list = []
-jmbg = []
-email = []
+jmbg_list = []
+email_list = []
 username_list.append('nikola123')
 password_list.append('passnidzo')
 ime_list.append('Nikola')
 prezime_list.append('Nikolic')
-jmbg.append(1234)
-email.append('nikolanikolic@gmail.com')
+jmbg_list.append(1234)
+email_list.append('nikolanikolic@gmail.com')
 username_list.append('pavleggez')
 password_list.append('passpavle')
 ime_list.append('Pavle')
 prezime_list.append('Pavlovic')
-jmbg.append(4567)
-email.append('pavle.pavlovic8@gmail.com')
+jmbg_list.append(4567)
+email_list.append('pavle.pavlovic8@gmail.com')
 username_list.append('alomilos')
 password_list.append('passmilos')
 ime_list.append('Milos')
 prezime_list.append('Milosevic')
-jmbg.append(3956)
-email.append('milos486@gmail.com')
+jmbg_list.append(3956)
+email_list.append('milos486@gmail.com')
 
 def rezervisi(br_karata):
     br_karata = int(br_karata)
@@ -49,21 +49,38 @@ def broadcast(message):
         client.send(msg.encode(FORMAT))
 
 def recieve_choice(client, address):
+    max_karata = 4
+    client.send('GREETINGIzaberite neku od opcija\nLIST - broj preostalih karata\nRESERVE - rezervisati kartu\nIZLAZ - za izlaz iz aplikacije'.encode(FORMAT))
+
     try:
         while True:
-            message = client.recv(1024).decode(FORMAT)
-            if message.upper() == 'LIST':
+            message = (client.recv(1024).decode(FORMAT)).upper()
+            if message == 'LIST':
                 client.send(f'LISTPreostalo je jos {BROJ_KARATA} slobodnih karata.'.encode(FORMAT))
-            elif message.upper()[:7] == 'RESERVE':
+            elif message[:7] == 'RESERVE':
                 unos = message[7:]
-                rezervisi(unos)
-                print(f'{address} je rezervisao {unos} karte! Broj preostalih karata je {BROJ_KARATA}.')
+                if max_karata != 0:
+                    if ((max_karata-int(unos))<=0):
+                        rezervisi(max_karata)
+                        client.send(f'ANNRezervisan je maksimalan broj preostalih karata koje ste mogli da rezervisete({max_karata})!'.encode(FORMAT))
+                        print(f'{address} je rezervisao {max_karata} karte! Broj preostalih karata je {BROJ_KARATA}.')
+                        max_karata = 0                    
+                    else:
+                        rezervisi(unos)
+                        max_karata -= int(unos)
+                        print(f'{address} je rezervisao {unos} karte! Broj preostalih karata je {BROJ_KARATA}.')
                 broadcast(f'Preostalo je jos {BROJ_KARATA} karata!')
                 print(len(clients))
-            elif message.upper() == 'IZLAZ':
+            elif message == 'IZLAZ':
                 print(f'{str(address)} se diskonektovao!')
                 client.close()
                 break
+            elif message == 'MAX_TICKETS':
+                if max_karata != 0:
+                    client.send((('MAX_TICKETS')+(str(max_karata))).encode(FORMAT))
+                else:
+                    client.send('ANNRezervisali ste maksimalan broj karata!'.encode(FORMAT))
+
     except:
         print(f'{str(address)} se diskonektovao!')
         client.close()
@@ -82,35 +99,98 @@ def login_provjera(username, password):
         result = False
     return result
 
+def check_username(username_temp):
+    for username in username_list:
+        if username == username_temp:
+            return True
+    return False
         
 def auth(client, address):
-    thread_dead = False
-    client.send('Povezani ste sa serverom!'.encode(FORMAT))
-    client.send('Izaberite neku od opcija\nlogin - za postojeci nalog\nregister - za registraciju\nizlaz - za izlaz iz aplikacije'.encode(FORMAT))
+    username_temp = str()
+    password_temp = str()
+    ime_temp = str()
+    prezime_temp = str()
+    jmbg_temp = int()
+    email_temp = str()
+    auth_complete = False
+    client.send('GREETINGPovezani ste sa serverom!\nIzaberite neku od opcija\nlogin - za postojeci nalog\nregister - za registraciju\nizlaz - za izlaz iz aplikacije'.encode(FORMAT))
+    while True:
+        try:
+            message_rcvd = client.recv(1024).decode(FORMAT)
 
-    message_recieved = client.recv(1024).decode(FORMAT)
-    if message_recieved.lower() == 'login':
-        client.send("Vase korisnicko ime: ".encode(FORMAT))
-        username = client.recv(1024).decode(FORMAT)
+            if message_rcvd[:3] == 'REG':
+                print('SUCCESS REG')
+                client.send('REG_U_NAMEUnesite Vase korisnicko ime:'.encode(FORMAT))
+            
+            if message_rcvd[:6] == 'U_NAME':
+                if len(message_rcvd) == 6:
+                    client.send('NEAUTORIZOVAN PRISTUP!'.encode(FORMAT))
+                    client.close()
+                    return
+                if check_username(message_rcvd[6:]):
+                    client.send('ANNIzabrali ste postojece korisnicko ime!'.encode(FORMAT))
+                    client.send('REG_U_NAMEUnesite drugacije korisnicko ime:'.encode(FORMAT))
+                else:
+                    print('SUCCESS U_NAME')
+                    username_temp = message_rcvd[6:]
+                    client.send('REG_PASSWORDUnesite Vasu sifru:'.encode(FORMAT))
 
-        client.send("Vasa lozinka: ".encode(FORMAT))
-        password = client.recv(1024).decode(FORMAT)
+            if message_rcvd[:8] == 'PASSWORD':
+                if len(message_rcvd) == 8:
+                    client.send('NEAUTORIZOVAN PRISTUP!'.encode(FORMAT))
+                    client.close()
+                    return
+                print('SUCCESS PASSWORD')
+                password_temp = message_rcvd[8:]
+                client.send('REG_NAMEUnesite Vase ime:'.encode(FORMAT))
+            
+            if message_rcvd[:4] == 'NAME':
+                if len(message_rcvd) == 4:
+                    client.send('NEAUTORIZOVAN PRISTUP!'.encode(FORMAT))
+                    client.close()
+                    return
+                print('SUCCESS NAME')
+                ime_temp = message_rcvd[4:]
+                client.send('REG_L_NAMEUnesite Vase prezime:'.encode(FORMAT))
 
-        check = login_provjera(username, password)
-        if check == False:
-            print("POGRESNI KREDENCIJALI")
+            if message_rcvd[:6] == 'L_NAME':
+                if len(message_rcvd) == 6:
+                    client.send('NEAUTORIZOVAN PRISTUP!'.encode(FORMAT))
+                    client.close()
+                    return
+                print('SUCCESS L_NAME')
+                prezime_temp = message_rcvd[6:]
+                client.send('REG_JMBGUnesite Vas jmbg:'.encode(FORMAT))
+            
+            if message_rcvd[:4] == 'JMBG':
+                if len(message_rcvd) == 4:
+                    client.send('NEAUTORIZOVAN PRISTUP!'.encode(FORMAT))
+                    client.close()
+                    return
+                print('SUCCESS JMBG')
+                jmbg_temp = int(message_rcvd[4:])
+                # print(jmbg_temp)
+                # print(type(jmbg_temp))
+                client.send('REG_EMAILUnesite Vasu e-mail adresu:'.encode(FORMAT))
+
+            if message_rcvd[:5] == 'EMAIL':
+                if len(message_rcvd) == 5:
+                    client.send('NEAUTORIZOVAN PRISTUP!'.encode(FORMAT))
+                    client.close()
+                    return
+                print('SUCCESS EMAIL')
+                email_temp = message_rcvd[5:]
+                client.send('ANNUspesno ste se registrovali na server!'.encode(FORMAT))
+                client.send('AUTH_SUCCESS'.encode(FORMAT))
+                auth_complete = True
+                break
+
+        except:
+            print(f'{str(address)} se diskonektovao!')
             client.close()
+            return
 
-    else:
-        thread_dead = True
-        client.send("Greska u unosu!")
-        print(f'{str(address)} se diskonektovao!')
-        client.close()
-        return
-
-
-
-    if thread_dead == False:
+    if auth_complete:
         recieve_thread = threading.Thread(target=recieve_choice, args=(client, address))
         recieve_thread.start()
 
@@ -119,9 +199,8 @@ def connection():
         client, address = server.accept()
         clients.append(client)
         print(f'Uspjesno povezan sa {str(address)}')
-        auth(client, address)
-        #client.send('GREETINGPovezani ste sa serverom!\nIzaberite neku od opcija\nLIST - broj preostalih karata\nRESERVE - rezervisati kartu\nIZLAZ - za izlaz iz aplikacije'.encode(FORMAT))
-
+        auth_thread = threading.Thread(target=auth, args=(client, address))
+        auth_thread.start()
 
 
 
